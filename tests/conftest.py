d8df0ce663effb3f19754af075f2553cd17ebbe7
Mohamed Awnallah <69568555+mhmdawnallah@users.noreply.github.com>
2023-01-26 18:34:54 +0200

    Delete tests directory

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from function_plotter import FunctionPlotter
-
-@pytest.fixture
-def function_plotter(qtbot):
-    plotter = FunctionPlotter()
-    qtbot.addWidget(plotter)
-    return plotter
